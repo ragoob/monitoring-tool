7b4a878197564a7d616da3e6a7f078b77e954005
Mohammed ragab <regoo707@gmail.com>
2020-10-30 20:36:07 +0200

    edit deployment script

--- linux-machines-monitoring-daemon/daemon-deploy.sh
+++ linux-machines-monitoring-daemon/daemon-deploy.sh
@@ -3,29 +3,55 @@ set -e
 
 ### Configuration ###
 
-APP_DIR=/var/www/docker-daemon
-GIT_URL=git://github.com/ragoob/docker-metrics-daemon
+APP_DIR=/home/$USER/
+GIT_URL=https://github.com/ragoob/monitoring-tool.git
 RESTART_ARGS=
-
+export SOCKET_SERVER=192.168.1.7:4000
+export MACHINE_ID=9b130e3a-3a33-5709-ab3e-d7642231d236
+export PORT=30003
+### Installing Node ###
+if which node > /dev/null
+    then
+        echo "node is installed, skipping..."
+    else
+      sudo apt-get -y install curl
+      curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+      sudo apt-get -y install nodejs
+    fi
 
 
 ### Automation steps ###
-
+rm -rf $APP_DIR/monitoring-tool
 set -x
 
-# Pull latest code
-if [[ -e $APP_DIR/docker-metrics-daemon ]]; then
-  cd $APP_DIR/docker-metrics-daemon
-  git pull
-else
-  git clone $GIT_URL $APP_DIR
-  cd $APP_DIR/docker-metrics-daemon
-fi
+# clone code from github
+ git clone $GIT_URL $APP_DIR
+ cd $APP_DIR/monitoring-tool/linux-machines-monitoring-daemon
 
 # Install dependencies
 npm install --production
 npm prune --production
+npm run build
 
-# Restart app
-passenger-config restart-app --ignore-app-not-running --ignore-passenger-not-running $RESTART_ARGS $APP_DIR/docker-metrics-daemon
 
+## create systemd service ####
+
+sudo nano cat > /lib/systemd/system/linux-machines-monitoring-daemon.service <<- "EOF"
+[Unit]
+Description=montoring tool agent to send data to dashboard
+Documentation=https://github.com/ragoob/monitoring-tool
+After=network.target
+
+[Service]
+Environment=NODE_PORT=30003
+Type=simple
+User=$USER
+ExecStart=/usr/bin/node $APP_DIR/monitoring-tool/linux-machines-monitoring-daemon/dist/main.js
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+ sudo systemctl daemon-reload
+ sudo systemctl start linux-machines-monitoring-daemon
