86d9f3aec176dcf6b38dccd309dac38ff47d8515
Mohammed ragab <regoo707@gmail.com>
2020-11-04 04:02:36 +0200

    edit charts

--- dashboard/src/environments/environment.prod.ts
+++ dashboard/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  socketServer: 'https://monitoring-gatway.mohammedragab.com',
+  gateWay: 'https://monitoring-gatway.mohammedragab.com'
 };
