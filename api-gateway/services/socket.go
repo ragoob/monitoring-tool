f0669b64d67b2bbd33033db8534563cada2ddfac
Mohammed Ragab <regoo707@gmail.com>
2020-12-12 06:50:42 -0500

    add events const

--- api-gateway/services/socket.go
+++ api-gateway/services/socket.go
@@ -8,6 +8,26 @@ import (
 	"github.com/graarh/golang-socketio/transport"
 )
 
+const (
+	CONTAINERS_LIST    = "CONTAINERS_LIST"
+	CONTAINERS_METRICS = "CONTAINERS_METRICS"
+	MEMORY_USAGE       = "MEMORY_USAGE"
+	DISK_USAGE         = "DISK_USAGE"
+	DOCKER_ENGINE_INFO = "DOCKER_ENGINE_INFO"
+	TEMPERATURE        = "TEMPERATURE"
+	HEALTH_CHECK       = "HEALTH_CHECK"
+	CONTAINER_START    = "CONTAINER_START"
+	CONTAINER_RESTART  = "CONTAINER_RESTART"
+	CONTAINER_STOP     = "CONTAINER_STOP"
+	CONTAINER_DELETE   = "CONTAINER_DELETE"
+	CONTAINER_DETAILS  = "CONTAINER_DETAILS"
+	DOCKER_RUN_IMAGE   = "DOCKER_RUN_IMAGE"
+	CPU_USAGE          = "CPU_USAGE"
+	ASK_CONTAINER_LOGS = "ASK_CONTAINER_LOGS"
+	CONTAINER_LOGS     = "CONTAINER_LOGS"
+	SUMMARY            = "SUMMARY"
+)
+
 //SocketService handler
 type SocketService struct{}
 
@@ -18,13 +38,13 @@ func (s SocketService) Handle(router *mux.Router) {
 		fmt.Println(c.Id() + " is Connected")
 	})
 
-	server.On("SUMMARY", func(c *gosocketio.Channel, msg string) {
+	server.On(SUMMARY, func(c *gosocketio.Channel, msg string) {
 
 		fmt.Println(msg)
 
 	})
 
-	server.On("TEMPERATURE", func(c *gosocketio.Channel, msg string) {
+	server.On(TEMPERATURE, func(c *gosocketio.Channel, msg string) {
 
 		fmt.Println(msg)
 
