423227506f8dad59036fc161c11f7c3f35de35c2
Mohammed Ragab <regoo707@gmail.com>
2020-12-12 07:01:26 -0500

    add events const

--- api-gateway/repository/machine/machine-repository.go
+++ api-gateway/repository/machine/machine-repository.go
@@ -37,3 +37,50 @@ func (m MachineRepository) GetMachine(db *sql.DB, machine models.Machine, id str
 
 	return machine, err
 }
+
+//AddMachine ..
+func (m MachineRepository) AddMachine(db *sql.DB, machine models.Machine) (string, error) {
+	err := db.QueryRow("insert into machines (name) values($1) RETURNING id;",
+		machine.Name).Scan(&machine.ID)
+
+	if err != nil {
+		return "", err
+	}
+
+	return machine.ID, nil
+}
+
+//UpdateMachine ..
+func (m MachineRepository) UpdateMachine(db *sql.DB, machine models.Machine) (int64, error) {
+	result, err := db.Exec("update machines set name=$1 where id=$4 RETURNING id",
+		&machine.Name, &machine.ID)
+
+	if err != nil {
+		return 0, err
+	}
+
+	rowsUpdated, err := result.RowsAffected()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return rowsUpdated, nil
+}
+
+//RemoveMachine ..
+func (m MachineRepository) RemoveMachine(db *sql.DB, id int) (int64, error) {
+	result, err := db.Exec("delete from machines where id = $1", id)
+
+	if err != nil {
+		return 0, err
+	}
+
+	rowsDeleted, err := result.RowsAffected()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return rowsDeleted, nil
+}
